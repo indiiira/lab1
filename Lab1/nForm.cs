debccc5043fb9656c9867a6676887e4104c7d9ca
indiiira <indira_shakirova2003@mail.ru>
2024-03-15 12:41:14 +0300

    lab1(1,2,3)

---
+++ Lab1/nForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class nForm : Lab1.Form2
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Hello!!!";
+            button1.BackColor = Color.Brown;
+        }
+    }
+}
